f33b7e4ea80c82644020bb721045a8f5b8515d36
Emily McEwen <emilymcewen@Emilys-MBP.lan>
2022-06-15 21:34:43 -0400

    first commit

---
+++ Assets/javascript/API_1.js
@@ -0,0 +1,17 @@
+
+//Psuedo Code for Movie Database API//
+
+/*GET popular movie data from the moviedatabase API (randomly pick number 1-1000 for the “page” parameter to traverse pages and get different results)
+
+Json the data and store the objects in an array of movies
+
+Loop through the array and check for specified genre ID (per documentation)
+
+Add event listener for genre buttons: when clicked check the movie array for objects containing genres ID and store in a “selected genre movie objects” array.
+
+randomly pick movies objects from the selected genre array by creating a random number function that will act as the arrays index
+
+After 5 non-repetitive results display the movie objects on the page
+7:53
+This api will allow us to access 1000 pages of the most popular movies updated daily and we will just need to specify which page/pages to use*/
+
