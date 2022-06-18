974af4df7cbe9400d35560d1fc970197e2591660
Mariah <mariahjohnston217@outlook.com>
2022-06-18 13:55:45 -0400

    updated card title class name, created var's in javascript for card titles

--- Assets/javascript/localstorage.js
+++ Assets/javascript/localstorage.js
@@ -1,5 +1,6 @@
 //Step 1: store which button was clicked into local storage.
 //Step 2: parse button that was clicked from local storage and put onto the page in <p>.
+//Step 3: make genre stay on the screen after refresh.
 
 function putDropDownValueIntoLocalStorage() {
     var selectText=document.getElementById("my-text").value;
@@ -10,6 +11,6 @@ function putDropDownValueIntoLocalStorage() {
 
 function getTextValueFromLocalStorageAndPutOntoPage(){
     document.getElementById("genre").innerHTML=localStorage.getItem("DropDownValue");
-
+    
 }
 
