50dd923b3a16e3ce2d13c265b306f021c8d28378
Mariah <mariahjohnston217@outlook.com>
2022-06-17 23:42:04 -0400

    created p tag and span id for last searched, created function to pull from local storage and put onto the page

--- Assets/javascript/localstorage.js
+++ Assets/javascript/localstorage.js
@@ -1,9 +1,15 @@
 //Step 1: store which button was clicked into local storage.
-//Step 2: parse button that was clicked from local storage and put onto the page.
+//Step 2: parse button that was clicked from local storage and put onto the page in <p>.
 
-function putDropDownValueIntoLocalStorage(){
-    var selectText = document.getElementById("my-text").value;
+function putDropDownValueIntoLocalStorage() {
+    var selectText=document.getElementById("my-text").value;
     localStorage.setItem("DropDownValue", selectText);
-    
+    getTextValueFromLocalStorageAndPutOntoPage();
     return true;
 }
+
+function getTextValueFromLocalStorageAndPutOntoPage(){
+    document.getElementById("genre").innerHTML=localStorage.getItem("DropDownValue");
+
+}
+
