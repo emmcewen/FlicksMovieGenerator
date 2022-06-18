baaffe58b4a3e112a065f9f9d93fdbaafa22bcc1
Mariah <mariahjohnston217@outlook.com>
2022-06-17 23:29:01 -0400

    created dropdown menu in html, added function for putting drop down value into local storage

--- Assets/javascript/localstorage.js
+++ Assets/javascript/localstorage.js
@@ -1,1 +1,9 @@
-//hi
+//Step 1: store which button was clicked into local storage.
+//Step 2: parse button that was clicked from local storage and put onto the page.
+
+function putDropDownValueIntoLocalStorage(){
+    var selectText = document.getElementById("my-text").value;
+    localStorage.setItem("DropDownValue", selectText);
+    
+    return true;
+}
