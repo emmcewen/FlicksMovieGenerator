9832a9770ba570f1714415970c7866d6766b5251
Emily McEwen <emilymcewen@Emilys-MBP.lan>
2022-06-20 16:49:59 -0400

    adding html updates an js

--- Assets/javascript/API_2.js
+++ Assets/javascript/API_2.js
@@ -6,34 +6,52 @@ Json the data and store the objects in an array of scores
 
 Loop through the array and check for streaming information (per documentation)*/
 
-//javascript jquery//
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest&page=1",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "5163f66adamsh9a5b7af41166208p183e37jsna4acff3a3926",
-		"X-RapidAPI-Host": "ott-details.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
 
 //javascript fetch//
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5163f66adamsh9a5b7af41166208p183e37jsna4acff3a3926',
-		'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
-	}
-};
 
-fetch('https://ott-details.p.rapidapi.com/advancedsearch?start_year=1970&end_year=2020&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=latest&page=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+
+var actionBtn = document.querySelector("#action")
+var romanceBtn = document.querySelector("#romance")
+var scifiBtn = document.querySelector("#scifi")
+var comedyBtn = document.querySelector("#comedy")
+var horrorBtn = document.querySelector("#horror")
+var card1El = document.querySelector(".card-1")
+var card2El = document.querySelector(".card-2")
+var card3El = document.querySelector(".card-3")
+var card4El = document.querySelector(".card-4")
+var card5El = document.querySelector(".card-5")
+var description1El = document.querySelector("#description-1")
+var description2El = document.querySelector("#description-2")
+var description3El = document.querySelector("#description-3")
+var description4El = document.querySelector("#description-4")
+var description5El = document.querySelector("#description-5")
+var comedyTitleArray = []
+var horrorTitleArray = []
+var scifiTitleArray = []
+var romanceTitleArray = []
+var actionTitleArray = []
+
+var promiseArray = []
+
+
+for (var i = 1; i < 6; i++) {
+	var cardSelect = document.getElementById("card-" + i).value
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '5163f66adamsh9a5b7af41166208p183e37jsna4acff3a3926',
+			'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
+
+		}
+	}
+
+	fetch('https://ott-details.p.rapidapi.com/search?title=' + cardSelect + '&page=1', options)
+		.then(response => response.json())
+		.then(function (data) {
+			data.results[0].imdbid=id;
+
+		}
+
+		.catch(err => console.error(err))
+		
+	}
--- Assets/javascript/localstorage.js
+++ Assets/javascript/localstorage.js
@@ -1,15 +1,47 @@
-//Step 1: store which button was clicked into local storage.
-//Step 2: parse button that was clicked from local storage and put onto the page in <p>.
-
-function putDropDownValueIntoLocalStorage() {
-    var selectText=document.getElementById("my-text").value;
-    localStorage.setItem("DropDownValue", selectText);
-    getTextValueFromLocalStorageAndPutOntoPage();
-    return true;
-}
-
-function getTextValueFromLocalStorageAndPutOntoPage(){
-    document.getElementById("genre").innerHTML=localStorage.getItem("DropDownValue");
-
-}
-
+for (i = 1; i < 100; i++) {
+    var prom = fetch("https://api.themoviedb.org/3/movie/popular?api_key=74202059f4cf77ba57c6c082dbb67f3d&language=en-US&page=" + i)
+        .then(response => response.json())
+        .then(function (data) {
+            for (var i = 0; i < data.results.length; i++) {
+                //genre ids
+                if (data.results[i].genre_ids) {
+                    //search for comedy
+                    if (data.results[i].genre_ids[0] === 35) {
+                        var descriptionData = data.results[i].overview
+                        var title = data.results[i].original_title
+                        comedyTitleArray.push(title)
+                        comedyDescrArray.push(descriptionData)
+                    }
+                    //search for action adventure
+                    else if (data.results[i].genre_ids[0] === 12 || data.results[i].genre_ids[0] === 28) {
+                        var descriptionData = data.results[i].overview
+                        var title = data.results[i].original_title
+                        actionTitleArray.push(title)
+                        actionDescrArray.push(descriptionData)
+                    }
+                    //search for horror
+                    else if (data.results[i].genre_ids[0] === 27) {
+                        var descriptionData = data.results[i].overview
+                        var title = data.results[i].original_title
+                        horrorTitleArray.push(title)
+                        horrorDescrArray.push(descriptionData)
+                    }
+                    //search for romance
+                    else if (data.results[i].genre_ids[0] === 10749) {
+                        var descriptionData = data.results[i].overview
+                        var title = data.results[i].original_title
+                        romanceTitleArray.push(title)
+                        romanceDescrArray.push(descriptionData)
+                    }
+                    //search for scifi
+                    else if (data.results[i].genre_ids[0] === 878) {
+                        var descriptionData = data.results[i].overview
+                        var title = data.results[i].original_title
+                        scifiTitleArray.push(title)
+                        scifiDescrArray.push(descriptionData)
+                    }
+                }
+            }
+        })
+        .catch(err => console.error(err))
+    }
