56c36fe04295fc2ee344d2b9fe88d712911dbe92
Mariah <mariahjohnston217@outlook.com>
2022-06-20 14:59:36 -0400

    changed onchange to displayMovie

--- Assets/javascript/localstorage.js
+++ Assets/javascript/localstorage.js
@@ -1,6 +1,6 @@
-//Step 1: store which button was clicked into local storage.
-//Step 2: parse button that was clicked from local storage and put onto the page in <p>.
-//Step 3: make genre stay on the screen after refresh.
+//Step 1: store which button was clicked into local storage. ✔️
+//Step 2: parse button that was clicked from local storage and put onto the page in <p>. ✔️
+//Step 3: make genre stay on the screen after refresh.✔️
 
 
 function putDropDownValueIntoLocalStorage() {
